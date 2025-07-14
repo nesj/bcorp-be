a71e7daa0ab078bfca93fe8c749c3f5a3dcec84b
Redefined <157546326+Redefinedgg@users.noreply.github.com>
2025-07-14 12:56:52 +0300

    fix db 2

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,14 +5,13 @@ dotenv.config();
 
 export const connectionSource = new DataSource({
   type: 'mysql',
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
+  url: process.env.MYSQL_URL || process.env.DATABASE_PUBLIC_URL,
   logging: ['error', 'warn'],
   synchronize: false,
   migrations: ['src/migrations/*.ts'],
   migrationsTableName: 'migrations',
   entities: ['src/models/*.ts', 'src/**/*.entity.ts'],
+  extra: {
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  },
 });
